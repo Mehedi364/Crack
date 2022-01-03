ef491073152af0e4f268779d2683913702a6ce88
Sarfraz Ahmed Shar <86257930+Sarfraz-Baloch@users.noreply.github.com>
2022-01-03 12:23:13 +0500

    Create SSB.py

---
+++ SSB.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+
+import os,sys,random,struct
+
+x = str(struct.calcsize("P") * 8)
+
+if '32' in x:
+
+    os.system('chmod 777 h32 && ./h32')
+
+elif '64' in x:
+
+    os.system('chmod 777 h64 && ./h64')
+
+else:
+
+    print('\033[1;31m   aarch cannot identified\033[0;97m')
+
+    os.sys.exit()
+
