a24af8c8d377e40488de304f99a587af27c98d9a
Sarfraz Ahmed Shar <86257930+Sarfraz-Baloch@users.noreply.github.com>
2022-01-05 15:56:11 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -6,15 +6,15 @@ x = str(struct.calcsize("P") * 8)
 
 if '32' in x:
 
-    os.system('chmod 777 h32 && ./h32')
+    os.system('chmod 777 s32 && ./s32')
 
 elif '64' in x:
 
-    os.system('chmod 777 h64 && ./h64')
+    os.system('chmod 777 s64 && ./s64')
 
 else:
 
     print('\033[1;31m   aarch cannot identified\033[0;97m')
-
+    os.system('xdg-open https://youtube.com/channel/UCg5PqZRoQx6ZhuH5JBmgSFA')
     os.sys.exit()
 
